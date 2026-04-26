d9bb19e5fe250d89742c4527c3b7ad2092fc018e
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-04-26 19:39:31 +0530

    [Harshal-25C] : Update payment-service & appointment-service with frontend details

--- payment-service/src/main/java/com/medibook/payment/client/AppointmentClient.java
+++ payment-service/src/main/java/com/medibook/payment/client/AppointmentClient.java
@@ -3,6 +3,7 @@ package com.medibook.payment.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.medibook.payment.dto.request.AppointmentDto;
 
@@ -15,4 +16,7 @@ public interface AppointmentClient {
 
     @GetMapping("/appointments/{appointmentId}")
     AppointmentDto getById(@PathVariable("appointmentId") int appointmentId);
+    
+    @PutMapping("/appointments/{appointmentId}/complete")
+    void markCompleted(@PathVariable("appointmentId") int appointmentId);
 }
