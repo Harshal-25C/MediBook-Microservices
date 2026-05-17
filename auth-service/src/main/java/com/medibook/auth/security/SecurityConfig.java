f59c73e9c7533e3daefe15096101e319178e94bc
Harshal Choudhary <harshalchoudhary340@gmail.com>
2026-05-17 12:58:01 +0530

    [Harshal-25C] : Fix CORS Origin Configurations

--- auth-service/src/main/java/com/medibook/auth/security/SecurityConfig.java
+++ auth-service/src/main/java/com/medibook/auth/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.medibook.auth.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -97,6 +98,7 @@ public class SecurityConfig {
                  * PDF explicitly requires guests to browse
                  * providers and slots without login.
                  */
+            	.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers(
                     "/auth/register",           // anyone can register
                     "/auth/login",              // anyone can login
